4a1f5bd9e6cf52aadc1d4839faa8dc1f5e0c0c72
Misha D <misha.dopre@viacesi.fr>
2025-12-01 14:32:57 +0100

    Ajout cellules

---
+++ include/Cell.h
@@ -0,0 +1,26 @@
+#ifndef CELL_H
+#define CELL_H
+
+#include "State.h"
+#include <memory>
+
+class Cell {
+private:
+    std::unique_ptr<CellState> etat;//etat de la cellule
+    std::unique_ptr<CellState> prochainEtat;//etat suivant
+
+public:
+    Cell(bool startAlive = false);//pour dire qu'une cellule est morte de base
+
+    Cell(const Cell& other);//constructeur de copie
+    Cell& operator=(const Cell& other);//operateur de copie
+
+    bool estVivant() const;
+    char symbole() const;
+
+    void prepareProchainEtat(std::unique_ptr<CellState> newState);
+    void majEtat();
+    const CellState& getState() const;
+};
+
+#endif
---
+++ src/Cell.cpp
@@ -0,0 +1,53 @@
+#include "../include/Cell.h"
+
+Cell::Cell(bool startAlive) {
+    if (startAlive) {
+        etat = std::make_unique<AliveState>();
+    } else {
+        etat = std::make_unique<DeadState>();
+    }
+    prochainEtat = etat->clone();
+}
+Cell::Cell(const Cell& other) {
+    etat = other.etat->clone();
+    if (other.prochainEtat) {
+        prochainEtat = other.prochainEtat->clone();
+    } else {
+        prochainEtat = nullptr;
+    }
+}
+
+
+Cell& Cell::operator=(const Cell& other) {
+    if (this != &other) {
+        etat = other.etat->clone();
+        if (other.prochainEtat) {
+            prochainEtat = other.prochainEtat->clone();
+        } else {
+            prochainEtat = nullptr;
+        }
+    }
+    return *this;
+}
+bool Cell::estVivant() const {
+    return etat->estVivant();
+}
+
+char Cell::symbole() const {
+    return etat->symbole();
+}
+
+
+void Cell::prepareProchainEtat(std::unique_ptr<CellState> newState) {
+    prochainEtat = std::move(newState);
+}
+void Cell::majEtat() {
+    if (prochainEtat) {
+        etat = std::move(prochainEtat);
+        prochainEtat = etat->clone();
+    }
+}
+
+const CellState& Cell::getState() const {
+    return *etat;
+}
